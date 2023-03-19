dc9cf7fa5cbd7fb02e6ecdd11470f0b9ec66e73f
taimul <taimul.haque@gmail.com>
2023-03-19 21:13:49 +0600

    login and register page created

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/system";
+import React from "react";
+import Navbar from "scenes/navbar";
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
+
+export default HomePage;
